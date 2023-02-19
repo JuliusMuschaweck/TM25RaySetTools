9ebb30b96877de6d6f7420444a8a66843dc3cc29
Julius Muschaweck <julius@zs66.de>
2023-02-19 16:08:02 +0100

    shuffle etc

--- TM25ReadWrite/TM25_impl.h
+++ TM25ReadWrite/TM25_impl.h
@@ -11,6 +11,7 @@ or http://unlicense.org/
 #include "TM25Util.h"
 #include <sstream>
 #include "TM25.h"
+#include <algorithm>
 namespace TM25
 	{
 
@@ -992,4 +993,17 @@ namespace TM25
 			std::memcpy(data_.data() + i * nItems_, ray.data(), sizeof(float) * N);
 			}
 	
+		template< typename Iter>
+		void TDefaultRayArray::SetRay(size_t i, Iter begin)
+			// copies ray into the i'th row, starting at begin, iterating over nItem floats
+			{
+			if (i >= nRays_)
+				{
+				std::stringstream s;
+				s << "TDefaultRayArray::SetRay<Iter>: i (" << i << ") >= NRays() (" << nRays_ << ")";
+				throw TM25Error(s.str());
+				}
+
+			const float* ibegin = &(*begin);
+			}
 	}
